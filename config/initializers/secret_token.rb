c4070917970b394258b38f4a7d551d184c49d62a
ruvazrael <marco.wittich@metaminded.com>
2011-11-24 10:13:27 +0100

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Documentr::Application.config.secret_token = '06573916c8f53ae7c58c08f2742f09b10a89520327c066a4f6ed0d54dcb16aac3361a2e920ed50d85b91bb65e358c585fecedfbb3dc4abbd222c2b9e8219c12a'
